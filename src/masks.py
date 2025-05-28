d4538cf89d586646828b25aedecf4561bcc0ae9d
Радин Александр Сергеевич <Aleksandr.Radin@geropharm.com>
2025-05-28 15:43:24 +0300

    add all files

---
+++ src/masks.py
@@ -0,0 +1,43 @@
+def get_mask_card_number(number_card: int) -> str:
+    """Функция маскировки номера банковской карты
+    """
+    mask_card_number = list(str(number_card))
+
+    for i, number in enumerate(mask_card_number):
+        if 5 < i < len(mask_card_number) - 4:
+            mask_card_number[i] = "*"
+
+    for i, number in enumerate(mask_card_number):
+        if (i + 1) % 4 == 0 and i != 0:
+            mask_card_number[i] = number + " "
+    new_mask_card_number = "".join(mask_card_number)
+
+    return new_mask_card_number
+
+
+def get_mask_account(number_card: int) -> str:
+    """Функция маскировки номера банковского счета
+    """
+    mask_account = "**" + str(number_card)[-4:]
+
+    return mask_account
+
+
+def mask_account_card(card_info: str) -> str:
+    """Функция обрабатывает информацию о картах и счетах
+    """
+    number_card = []
+    name_card = []
+    for char in card_info:
+        if char.isdigit():
+            number_card.append(char)
+        else:
+            name_card.append(char)
+
+    number_card = ''.join(number_card)
+    name_card = ''.join(name_card)
+    if len(number_card) == 16:
+        inform_about_card = name_card + get_mask_card_number(number_card)
+    elif len(number_card) == 20:
+        inform_about_card = name_card + get_mask_account(number_card)
+    return inform_about_card
