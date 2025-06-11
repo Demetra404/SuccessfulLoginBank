7467a51386fffcbb61d4b8bff074f830f43db963
demetra <jennyani98@gmail.com>
2025-06-11 16:34:33 +0300

    add function sort_by_date in processing.py

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,7 @@ def filter_by_state(bank_info, state='EXECUTED'):
 
     return list_state
 
+def sort_by_date(not_sorted_info, sort=True):
+    sorted_info = sorted(not_sorted_info, key=lambda date: date.get('date'), reverse=sort)
+
+    return sorted_info
