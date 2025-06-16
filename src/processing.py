5674c28bf71d1281921b912f9af984068b17722c
demetra <jennyani98@gmail.com>
2025-06-16 22:09:45 +0300

    add types and description

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,6 @@
-from typing import Union
-
-def filter_by_state(bank_info: list[dict[str, Union[str, int]]], state: str='EXECUTED') -> list[dict[str, Union[str, int]]]:
+def filter_by_state(bank_info: list[dict], state: str = 'EXECUTED') -> list[dict]:
+    """Функция возвращает новый список словарей, с указанным ключом state
+    """
     list_state = []
     for index, info in enumerate(bank_info):
         if info.get('state') == state:
@@ -8,7 +8,10 @@ def filter_by_state(bank_info: list[dict[str, Union[str, int]]], state: str='EXE
 
     return list_state
 
-def sort_by_date(not_sorted_info: list[dict[str, Union[str, int]]], sort: bool=True) -> list[dict[str, Union[str, int]]]:
+
+def sort_by_date(not_sorted_info: list[dict], sort: bool = True) -> list[dict]:
+    """Функция возврает список отсортированный по дате
+    """
     sorted_info = sorted(not_sorted_info, key=lambda date: date.get('date'), reverse=sort)
 
     return sorted_info
