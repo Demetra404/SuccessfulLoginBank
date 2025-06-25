51467324c1567f18ec7d200890d633eb404894b3
demetra <jennyani98@gmail.com>
2025-06-25 16:40:48 +0300

    testsv2

---
+++ tests/test_masks.py
@@ -0,0 +1,18 @@
+import pytest
+from src.masks import get_mask_card_number, get_mask_account
+
+assert get_mask_card_number(7000792289606361) == '7000 79** **** 6361'
+assert get_mask_card_number(10000000000000000000) == '1000 00** **** **** 0000'
+assert get_mask_card_number(0) == 0
+
+assert get_mask_account(73654108430135874305) == '**4305'
+assert get_mask_account(9393999999999999999999999999999999999999999999999) == '**9999'
+assert get_mask_account(11555510) == 0
+
+@pytest.mark.parametrize('test_mask_card, positive_mask_card', [(7000792289606361, '7000 79** **** 6361'), (10000000000000000000, '1000 00** **** **** 0000'), (0, 0)])
+def test_get_mask_card_number(test_mask_card, positive_mask_card):
+    assert get_mask_card_number(test_mask_card) == positive_mask_card
+
+@pytest.mark.parametrize('test_mask_account, positive_account', [(73654108430135874305, '**4305'), (9393999999999999999999999999999999999999999999999, '**9999'), (11555510, 0)])
+def test_get_mask_account(test_mask_account, positive_account):
+    assert get_mask_account(test_mask_account) == positive_account
---
+++ tests/test_widget.py
@@ -0,0 +1,14 @@
+import pytest
+from src.widget import mask_account_card,get_date
+
+@pytest.mark.parametrize('account_card, mask_card', [('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),('Счет 64686473678894779589', 'Счет **9589'),('MasterCard 7158300734726758', 'MasterCard 7158 30** **** 6758'),('Счет 35383033474447895560', 'Счет **5560'), ('', 0), ('adadsefw 35383033474447895560', 'adadsefw **5560')])
+def test_mask_account_card(account_card: str, mask_card: str):
+    assert mask_account_card(account_card) == mask_card
+def test_digit_info():
+    with pytest.raises(ValueError) as digit_info:
+        mask_account_card(1)
+    assert str(digit_info.value) == "Информация о карте или счёте должна быть строкой"
+
+@pytest.mark.parametrize('date_info, date_need', [('2024-03-11T02:26:18.671407', '11.03.2024'),('2023-11-12T06:36:20.654307', '12.11.2023'), ('', 0)])
+def test_get_date(date_info, date_need):
+    assert get_date(date_info) == date_need
