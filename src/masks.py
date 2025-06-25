08d6073bd5c223a8550c9362fbc1f2e6543ea85d
demetra <jennyani98@gmail.com>
2025-06-25 16:37:45 +0300

    make tests for all functions

--- src/masks.py
+++ src/masks.py
@@ -1,22 +1,26 @@
-def get_mask_card_number(number_card: int) -> str:
+def get_mask_card_number(number_card: int) -> str | int:
     """Функция маскировки номера банковской карты
     """
-    mask_card_number = list(str(number_card))
+    if number_card > 0:
+        mask_card_number = list(str(number_card))
 
-    for i, number in enumerate(mask_card_number):
-        if 5 < i < len(mask_card_number) - 4:
-            mask_card_number[i] = "*"
+        for i, number in enumerate(mask_card_number):
+            if 5 < i < len(mask_card_number) - 4:
+                mask_card_number[i] = "*"
 
-    for i, number in enumerate(mask_card_number):
-        if (i + 1) % 4 == 0 and i != 0:
-            mask_card_number[i] = number + " "
-    new_mask_card_number = "".join(mask_card_number)
+        for i, number in enumerate(mask_card_number):
+            if (i + 1) % 4 == 0 and i != 0 and i < len(mask_card_number)-1:
+                mask_card_number[i] = number + " "
+        new_mask_card_number = "".join(mask_card_number)
 
-    return new_mask_card_number
+        return new_mask_card_number
+    return 0
 
-def get_mask_account(number_card: int) -> str:
+def get_mask_account(number_card: int) -> str | int:
     """Функция маскировки номера банковского счета
     """
-    mask_account = "**" + str(number_card)[-4:]
+    if len(str(number_card)) > 19:
+        mask_account = "**" + str(number_card)[-4:]
 
-    return mask_account
+        return mask_account
+    return 0
--- src/widget.py
+++ src/widget.py
@@ -1,35 +1,40 @@
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(card_info: str) -> str:
+def mask_account_card(card_info: str) -> str | int:
     """Функция обрабатывает информацию о картах и счетах
     """
     number_card = []
     name_card = []
+    if isinstance(card_info, (int, float)):
+        raise ValueError("Информация о карте или счёте должна быть строкой")
+    if card_info > '':
+        for char in card_info:
+            if char.isdigit():
+                number_card.append(char)
+            else:
+                name_card.append(char)
 
-    for char in card_info:
-        if char.isdigit():
-            number_card.append(char)
-        else:
-            name_card.append(char)
+        new_number_card = ''.join(number_card)
+        new_name_card = ''.join(name_card)
 
-    new_number_card = ''.join(number_card)
-    new_name_card = ''.join(name_card)
+        if len(number_card) == 16:
+            inform_about_card = new_name_card + str(get_mask_card_number(int(new_number_card)))
+        elif len(number_card) == 20:
+            inform_about_card = new_name_card + str(get_mask_account(int(new_number_card)))
 
-    if len(number_card) == 16:
-        inform_about_card = new_name_card + get_mask_card_number(int(new_number_card))
-    elif len(number_card) == 20:
-        inform_about_card = new_name_card + get_mask_account(int(new_number_card))
-
-    return inform_about_card
-
-def get_date(date: str) -> str:
+        return inform_about_card
+    return 0
+def get_date(date: str) -> str | int:
     """Функция изменяет формат даты на ДД.ММ.ГГГГ
     """
-    number = date[8:10]
-    month = date[5:7]
-    year = date[0:4]
+    date = str(date)
+    if date > '':
+        number = date[8:10]
+        month = date[5:7]
+        year = date[0:4]
 
-    new_format_date = f"{number}.{month}.{year}"
+        new_format_date = f"{number}.{month}.{year}"
 
-    return new_format_date
+        return new_format_date
+    return 0
