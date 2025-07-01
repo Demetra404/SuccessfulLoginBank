be3219e663ca3fcc0148643d686c8aa67e8aecce
demetra <jennyani98@gmail.com>
2025-07-01 13:01:59 +0300

    made new modul generators.py with generators

---
+++ src/generators.py
@@ -0,0 +1,22 @@
+def filter_by_currency(transactions, currency):
+    return  (transaction for transaction in transactions if transaction['operationAmount']['currency'].get('code') == currency)
+
+
+def transaction_descriptions(transactions):
+    if transactions:
+        return (transaction.get('description') for transaction in transactions if transaction.get('description'))
+    return 0
+
+
+def card_number_generator(start, stop):
+    start = start-1
+    while start < stop:
+        start+=1
+        card_number = str(start)
+        card_number_gen = list(''.join(['0'] * (16 - len(card_number))) + card_number)
+        for i, number in enumerate(card_number_gen):
+            if (i + 1) % 4 == 0 and i != 0 and i < len(card_number_gen)-1:
+                card_number_gen[i] = number + " "
+        new_card_number_gen = "".join(card_number_gen)
+
+        yield new_card_number_gen
--- src/widget.py
+++ src/widget.py
@@ -4,6 +4,7 @@ from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(card_info: str) -> str | int:
     """Функция обрабатывает информацию о картах и счетах
     """
+    inform_about_card = ''
     number_card = []
     name_card = []
     if isinstance(card_info, (int, float)):
